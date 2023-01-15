f5737cc6561d01bdd7d6fb8f8d30486cb1c271f0
Sven-D <sd_spam@outlook.de>
2023-01-15 19:25:08 +0100

    Test_Code included

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from pathlib import Path
-import textblob 
+from textblob import TextBlob
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 
-
+def getSentiment(text):
+    sentiment = TextBlob(text.content).sentiment.polarity
+    return float(sentiment)
 
 tweet_path = Path('C:/Users/Sven/Desktop/replies_to_elon.json')
 
@@ -13,11 +15,10 @@ tweets = spark.read.json(str(tweet_path))
 
 #removing unnecessary details like @ (maybe something else to remove?)
 tweets = tweets.withColumn("content",F.regexp_replace("content", "(@[a-zA-Z0-9]*)", ""))
+tweets = tweets.select(["content"])
 
+rdd2=tweets.rdd.map(lambda x: getSentiment(x)) 
 
-tweets.show()
-tweets.select("content").show()
-
-
-
+df2=rdd2.toDF(["content"])
+df2.show()
 
